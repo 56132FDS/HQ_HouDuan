306b71016766d6c7f68adf09dbb7a5d29ab164b9
Larrian <3166701497@qq.com>
2024-06-26 17:29:29 +0800

    项目结构

---
+++ HQ_HouDuan/src/main/java/org/example/hq_houduan/HqHouDuanApplication.java
@@ -0,0 +1,13 @@
+package org.example.hq_houduan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HqHouDuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HqHouDuanApplication.class, args);
+    }
+
+}
---
+++ HQ_HouDuan/src/test/java/org/example/hq_houduan/HqHouDuanApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hq_houduan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HqHouDuanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
